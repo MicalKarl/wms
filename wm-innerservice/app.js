dfc78db1b2e85fdcee2950b7f895dc1d12affd4c
karl <you@example.com>
2023-09-26 04:09:20 -0400

    fix&upgrade

--- wm-innerservice/app.js
+++ wm-innerservice/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const { set } = require('express/lib/application');
-const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 const path = require('path');
 const app = express()
 const port = 3000
 var cors = require('cors')
+const axios = require('axios/dist/node/axios.cjs')
 app.use(cors()) // Use this after the variable declaration
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, "views"));
@@ -31,7 +31,8 @@ wmLoginData = {
 rndCode = {
     code: undefined,
     ctime: undefined,
-    reqServerTime: undefined
+    reqServerTime: undefined,
+    nextUpdateTime: undefined
 }
 
 function genCode(n) {
@@ -50,64 +51,56 @@ function getCookie(s, key) {
     return s.substring(ix + key.length, ix2);
 }
 
-const request = require('request');
-
 function genQrCode() {
     qrdata.ctime = Date.now();
     const url = `https://qr.sec.wanmei.net:20176/qrservice/v1.0/show?t=${qrdata.ctime}&rcode=${genCode(6)}`;
-    try{
-        request(url, { json: true }, (err, res, body) => {
-            if (err) {
-                console.log(`fetch ${url} err: ${err}`);
-            } else {
-                qrdata.data = body;
-                const sck = res.headers["set-cookie"].join(';');
-                qrdata.wpollre_scred = getCookie(sck, 'wpollre_scred=');
-                qrdata.QRKey = getCookie(sck, 'QRKey=');
-            }
-        });
-    }catch(reqErr)
-    {
-        console.log(`req ${url} err: ${reqErr}}`);
-    }
+    axios(url, { json: true, resolveWithFullResponse: true }).then((res) => {
+        qrdata.data = res.data;
+        const sck = res.headers["set-cookie"].join(';');
+        qrdata.wpollre_scred = getCookie(sck, 'wpollre_scred=');
+        qrdata.QRKey = getCookie(sck, 'QRKey=');
+    }).catch(err => {
+        console.log(`get qrCode ${url} err: ${err}`);
+    });
 }
 
 
 function wmLogin(code) {
     if (qrdata.wpollre_scred != undefined && code == rndCode.code && code !== undefined || wmLoginData.wpollre_scred != qrdata.wpollre_scred) {
         const url = `https://qr.sec.wanmei.net:20176/qrservice/v1.0/check?t=${Date.now()}&token=${qrdata.wpollre_scred}`;
-        try {
-            (async () => {
-                const res = await fetch(url, {
-                    headers: {
-                        cookie: `QRKey=${qrdata.QRKey}; ec=HQvIykdU-1691475000578-14282591e3edd-819412952; i18next=cn;wpollre_scred=${qrdata.wpollre_scred}; _efmdata=7Az08O31SuuPpgdBEDMA%2BkgYB7mktxYXPEoOx6WOvJPWJq2mH3oz0FJvMLE0Xt0L555%2BcTH6vXJUaLAaWlqfZQ%3D%3D; _exid=iTqBJJm905s7IAF5xQkj6aIGQT5yerUKIp3FnlbiO8A%3D`
-                    }
-                });
-                const data = await res.json();
-                if (res.status == 200 && data.status == 200) {
-                    wmLoginData.loginDate = Date.now();
-                    wmLoginData.iPlanetDirectoryPro = data.tokenId;
-                    wmLoginData.wpollre_scred = qrdata.wpollre_scred;
-                } else {
-                    wmLoginData.iPlanetDirectoryPro = undefined;
-                }
-            })();
-        } catch (err) {
-            console.log(`wmLogin error: ${err}`);
-        }
+        axios(url, {
+            headers:
+            {
+                Cookie: `QRKey=${qrdata.QRKey}; ec=HQvIykdU-1691475000578-14282591e3edd-819412952; i18next=cn;wpollre_scred=${qrdata.wpollre_scred}; _efmdata=7Az08O31SuuPpgdBEDMA%2BkgYB7mktxYXPEoOx6WOvJPWJq2mH3oz0FJvMLE0Xt0L555%2BcTH6vXJUaLAaWlqfZQ%3D%3D; _exid=iTqBJJm905s7IAF5xQkj6aIGQT5yerUKIp3FnlbiO8A%3D`
+            },
+            json: true,
+            resolveWithFullResponse: true
+        }).then(res => {
+            const data = res.data;
+            if (res.status == 200 && data.status == 200) {
+                wmLoginData.loginDate = Date.now();
+                wmLoginData.iPlanetDirectoryPro = data.tokenId;
+                wmLoginData.wpollre_scred = qrdata.wpollre_scred;
+            } else {
+                wmLoginData.iPlanetDirectoryPro = undefined;
+            }
+        }).catch(err => {
+            console.log(`login ${url} err: ${err}`);
+        });
     }
 }
 
-function genRndCode() {
-    if (rndCode.ctime == undefined || Date.now() - rndCode.ctime > QrTimeMs) {
+function genRndCode(force=false) {
+    if (force || rndCode.ctime == undefined || Date.now() - rndCode.ctime > QrTimeMs) {
         rndCode.code = genCode(4);
         rndCode.ctime = Date.now();
+        rndCode.nextUpdateTime = rndCode.ctime + QrTimeMs + 1;
     }
     rndCode.reqServerTime = Date.now();
 }
 
 const _qrInterval = setInterval(() => {
-    genRndCode();
+    genRndCode(true);
     genQrCode();
     // 
     wmLoginData.iPlanetDirectoryPro = undefined;
@@ -136,9 +129,10 @@ app.get("/rndcode", (req, res) => {
 app.post("/login", (req, res) => {
     if (req.query.code != rndCode.code) {
         res.send('code not correct:)');
+    } else {
+        wmLogin(req.query.code);
+        res.send('ok!!!');
     }
-    wmLogin(req.query.code);
-    res.send('ok!!!');
 })
 
 app.get("/login", (req, res) => {
@@ -164,10 +158,20 @@ app.get("/logout", (req, res) => {
     }
 })
 
+appStartTimeStamp = undefined;
+app.get('/runtime', (req, res) => {
+    if (appStartTimeStamp != undefined) {
+        return res.json(Date.now() - appStartTimeStamp);
+    } else {
+        return res.json(-1);
+    }
+});
+
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
 
 app.listen(port, () => {
     console.log(`wms listening on port ${port}`)
     genQrCode();
+    appStartTimeStamp = Date.now();
 })
